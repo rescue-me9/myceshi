db76063f6fe97dbf344de98b3fd12e998e5da678
rescue-me9 <3957745056@qq.com>
2026-06-28 11:42:28 +0800

    首次提交

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "127.0.0.1";
+// MySQL Database User
+$sql_db_user = "yy";
+// MySQL Database Password
+$sql_db_pass = "yy";
+// MySQL Database Name
+$sql_db_name = "yy";
+
+// Site URL
+$site_url = "http://192.168.10.22"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "123456"; // Your purchase code, don't give it to anyone.
+?>
